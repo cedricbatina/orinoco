c5cad2c739552bb2b6cba899155340bce4375985
Cedric Batina <shakagarvey@gmail.com>
2020-10-12 11:26:14 +0200

    almost last commit

--- panier.js
+++ panier.js
@@ -52,27 +52,28 @@ contactFormular.setAttribute("novalidate", "");
 const formularFieldset = document.getElementById("formularFieldset");
 const formularLegend = document.getElementById("legend");
 formularLegend.innerHTML = "Formulaire de contact";
-
 // code to check the regex
 //vérifie les inputs du formulaire
-checkInput = () => {
+checkInputs = () => {
   //Controle Regex
   let nameRegex = /[a-zA-Z]/;
   let numberRegex = /[0-9]/;
   //Source pour vérification email => emailregex.com
   let emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/y;
   let specialRegex = /[§!@#$%^&*(),.?":{}|<>]/;
-
-  //message fin de controle
   let checkMessage = "";
-
   //Récupération des inputs
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;
   let address = document.getElementById("address").value;
   let city = document.getElementById("city").value;
   let email = document.getElementById("email").value;
-
+  let checkMessageFirstName = document.getElementById("firstNameError")
+  let checkMessagelastName = document.getElementById("lastNameError")
+  let checkMessageCityError = document.getElementById("cityError")
+  let checkMessageAddressError = document.getElementById("addressError")
+  let checkMessageEmailError = document.getElementById("emailError")
+  let checkCount = 0
   //tests des différents input du formulaire
   //Test du nom => aucun chiffre ou charactère spécial permis
   if (
@@ -80,47 +81,51 @@ checkInput = () => {
     specialRegex.test(firstName) == true ||
     firstName == ""
   ) {
-    checkMessage = "Vérifier/renseigner votre nom";
+    checkMessageFirstName.textContent = " Vérifier votre prénom";
   } else {
-    console.log("Administration : firstName ok");
+    checkCount++
   }
   //Test du nom => aucun chiffre ou charactère spécial permis
   if (
     numberRegex.test(lastName) == true ||
-    specialRegex.test(formPrenom) == true ||
+    specialRegex.test(lastName) == true ||
     lastName == ""
   ) {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre Nom";
+    checkMessagelastName.textContent = " Vérifier votre Nom";
   } else {
-    console.log("Administration : lastName ok");
+        checkCount++
+
   }
   //Test du mail selon le regex de la source L256
   if (emailRegex.test(email) == false) {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre email";
+    checkMessageEmailError.textContent = " Vérifier votre email";
   } else {
-    console.log("Administration : email ok");
+        checkCount++
   }
   //Test de l'adresse => l'adresse ne contient pas obligatoirement un numéro de rue mais n'a pas de characteres spéciaux
-  if (specialRegex.test(address) == true || address == "") {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre adresse";
+  if (specialRegex.test(address) == false || address == "") {
+    checkMessageAddressError.textContent = " Vérifier votre adresse";
   } else {
-    console.log("Administration : Adresse ok");
+        checkCount++
   }
   //Test de la ville => aucune ville en France ne comporte de chiffre ou charactères spéciaux
   if (
     (specialRegex.test(city) == true && checkNumber.test(city) == true) ||
     city == ""
   ) {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre ville";
+    checkMessageCityError.textContent = " Vérifier votre ville";
   } else {
-    console.log("Administration : Ville ok");
+        checkCount++
   }
-  //Si un des champs n'est pas bon => message d'alert avec la raison
-  if (checkMessage != "") {
-    alert("Il est nécessaire de :" + "\n" + checkMessage);
+  if (checkCount === 5)
+  {
+    return true
+  }
+  else{
+    return false
   }
 };
-// here ends copied code to check the regex
+// here ends code to check the regex
 // here ends the formular
 // creating a button to submit
 const formularButton = document.createElement("button");
@@ -131,7 +136,6 @@ formularButton.classList.add("btn");
 formularButton.addEventListener("click", function (event) {
   return setButton(event);
 });
-
 // here ends code for submit button
 // down below we find the functions
 //function to suppress camera
@@ -165,7 +169,10 @@ function generateTbody(i) {
 //function for submition  ////////got many problems to match my form
 function setButton(event) {
   event.preventDefault();
-  //checkInputs();
+  const checkValid = checkInputs();
+  if (!checkValid){
+    return;
+  }
   let contact = {
     firstName: document.getElementById("firstName").value,
     lastName: document.getElementById("lastName").value,
@@ -183,56 +190,12 @@ function setButton(event) {
   })
     .then((res) => res.json())
     .then((res) => {
-      console.log(res.contact);
       window.location.href =
         "confirmation.html?id=" + res.orderId + "&total=" + total;
     })
     .catch(function (res) {
-      console.log(res);
-      console.log(body);
-      console.log(contact);
+     alert(res)
     });
 }
 
-// creating a function to generate error
-/*function setErrorFor(input, message) {
-  formControl.classList.add("danger");
-  let error = document.getElementsByClassName("error");
-  firstNameError.innerHTML = message;
-}
-// creating a function to generate success
-function setSuccessFor(input) {
-  formControl.classList.add("success");
-}
-//creating a function to check the inputs
-function checkInputs() {
-  let status = false;
-  if (firstNameRegex == true) {
-    status = setSuccessFor(firstNameInput);
-  } else {
-    status = setErrorFor(
-      firstNameInput,
-      "Veuillez renseigner un prénom valide"
-    );
-  }
-  if (lastNameRegex == true) {
-    status = setSuccessFor(lastNameInput);
-  } else {
-    status = setErrorFor(lastNameInput, "Ce champ n'est pas valide");
-  }
-  if (cityRegex == true) {
-    status = setSuccessFor(city);
-  } else {
-    status = setErrorFor(cityInput, "Ce champ n'est pas valide");
-  }
-  if (addressRegex == true) {
-    status = setSuccessFor(addressInput);
-  } else {
-    status = setErrorFor(addressInput, "Ce champ n'est pas valide");
-  }
-  if (emailRegex == true) {
-    status = setSuccessFor(city);
-  } else {
-    status = setErrorFor(emailInput, "Ce champ n'est pas valide");
-  }
-}*/
+
